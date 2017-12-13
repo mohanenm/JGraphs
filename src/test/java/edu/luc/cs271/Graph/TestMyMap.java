2d8ab0cedd53d4a6b513ee4a9c5f92df7d8a8390
Ethan M <emohan@luc.edu>
2017-12-13 04:25:00 +0000

    final lab

--- src/test/java/edu/luc/cs271/Graph/TestMyMap.java
+++ src/test/java/edu/luc/cs271/Graph/TestMyMap.java
@@ -1,1 +1,30 @@
 
+mport java.util.*;
+
+public static void main(String[] args){
+   Graph g = null;
+   int n = 0;
+   try{
+       Scanner scan = new Scanner(new File(args[0]));
+       g = AbstractGraph.createGraph(scan, true, "List");
+       n = g.getNumV();
+   }
+   catch (IOException ex) {
+       ex.printStackTrace();
+       System.exit(1);
+   }
+   
+   breadthFirstSearch dfs = new depthFirstSearch(g);
+   int[] dOrder = dfs.getmyMap();
+   int[] fOrder = dfs.getmyMap();
+   for(int i = 0; i<n; i++) {
+       System.out.println(dOrder[i] + " " + fOrder[i]);
+   }
+   
+   depthFirstSearch dfs = new depthFirstSearch(g);
+   int[] dOrder = dfs.getmyMap();
+   int[] fOrder = dfs.getmyMap();
+   for(int i = 0; i<n; i++) {
+       System.out.println(dOrder[i] + " " + fOrder[i]);
+   }
+}
